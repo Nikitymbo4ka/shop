0dedc0a1bca7a345972aef567fb95a38fce1bc32
Nikitymbo4ka <104885333+Nikitymbo4ka@users.noreply.github.com>
2022-11-19 11:45:37 +0500

    Add files via upload

---
+++ views/site/contact.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var yii\bootstrap5\ActiveForm $form */
+/** @var app\models\ContactForm $model */
+
+use yii\bootstrap5\ActiveForm;
+use yii\bootstrap5\Html;
+use yii\captcha\Captcha;
+
+
+?>
+<div class="site-contact d-flex justify-content-between">
+    <div class="kart"><img src="../images/карта.png" alt=""></div>
+<div class="contact"><h2>Курган</h2>
+<h2>Улица Коли мяготина 96</h2>
+<h2>+7-912-575-12-34</h2>
+<h2>Nightwhite@email.ru</h2>
+</div>
+
+</div>
